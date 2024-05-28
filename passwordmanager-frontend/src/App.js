a375ad6e10072e3d5d01fb590303ee149e67d72f
avimax37 <avimax37@gmail.com>
2024-05-28 10:50:21 +0530

    First Commit

---
+++ passwordmanager-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Test1 from "./pages/Test1";
+import Login from "./pages/Login";
+
+
+const App = () => {
+  return (
+    <>
+      {/* <Test1 /> */}
+      <Login/>
+    </>
+  );
+};
+
+export default App;
---
+++ passwordmanager-frontend/src/pages/Test1.js
@@ -0,0 +1,10 @@
+import {Box, TextField} from "@mui/material"
+import React, {useEffect, useState} from "react"
+
+const Test1 = () => {
+    const [text1, setText1] = useState("")
+    const [text2, setText2] = useState("")
+    return (
+        <title>Login</title>
+    )
+}
